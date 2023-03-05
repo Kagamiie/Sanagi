cacfb63cdbb2c1eb05fe2f5e7797f54c8ef7c552
Kagami <89704351+Kagamiie@users.noreply.github.com>
2023-03-06 00:16:14 +0100

    Add files via upload

---
+++ REQUIREMENT.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo Installation des packages externes pour Python...
+
+pip install discord 
+pip install re
+pip install asyncio
+pip install requests
+
+echo Packages installés avec succès !
+pause
